ee4341bc2c1381cf152c3dbc3527df8b9d294e2d
jameschritianw <jamesch888@gmail.com>
2019-10-30 13:34:17 +0700

    Init week 10

---
+++ umn-homes_w10/src/app/places/offers/new-offer/new-offer.page.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PlacesService } from '../../places.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-new-offer',
+  templateUrl: './new-offer.page.html',
+  styleUrls: ['./new-offer.page.scss'],
+})
+export class NewOfferPage implements OnInit {
+
+  form: FormGroup
+
+  constructor(private placesSvc: PlacesService, private router: Router) { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      price: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    })
+  }
+
+  onCreateOffer(){
+    console.log("Creating offered place...")
+    // console.log(this.form)
+    // console.log(this.form.get('price').value)
+
+    if(!this.form.valid){
+      return
+    }
+
+    this.placesSvc.addPlace(
+      this.form.value.title,
+      this.form.value.description,
+      +this.form.value.price,
+      new Date(this.form.value.dateFrom),
+      new Date(this.form.value.dateTo)
+    )
+
+    this.form.reset();
+    this.router.navigate(['/places/tabs/offers'])
+    console.log(this.form)
+  }
+}
