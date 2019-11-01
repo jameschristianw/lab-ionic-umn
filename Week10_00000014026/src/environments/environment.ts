1798a8609649e74f61bd2bb9ad2be7d9a59854f2
James Christian Wira <39824958+pottato22@users.noreply.github.com>
2019-11-01 14:25:45 +0700

    test config

--- Week10_00000014026/src/environments/environment.ts
+++ Week10_00000014026/src/environments/environment.ts
@@ -1,19 +1,12 @@
+import { ApiConfig } from './config';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyB9kfyZVHPvACeUGFkvetsO_9YwOEmIOgA",
-    authDomain: "mcp-week10.firebaseapp.com",
-    databaseURL: "https://mcp-week10.firebaseio.com",
-    projectId: "mcp-week10",
-    storageBucket: "mcp-week10.appspot.com",
-    messagingSenderId: "725888123903",
-    appId: "1:725888123903:web:29d8832d584bde54565dca",
-    measurementId: "G-HL0CFXNLF6"
-  }
+  firebaseConfig: ApiConfig
 };
 
 /*
