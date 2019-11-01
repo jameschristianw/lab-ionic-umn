9ffb7cd389f722a1a0b15dc213877daa9dbef86e
James Christian Wira <39824958+pottato22@users.noreply.github.com>
2019-11-01 14:00:51 +0700

    Week 1 ga tau selesai atau ga

---
+++ MobileCrossPlatform_Week01/app.js
@@ -0,0 +1,12 @@
+async function presentAlert() {
+  const alertController = document.querySelector('ion-list');
+  await alertController.componentOnReady();
+
+  const alert = await alertController.create({
+    header: 'Alert',
+    subHeader: 'Subtitle',
+    message: 'This is an alert message.',
+    buttons: ['OK']
+  });
+  return await alert.present();
+}
